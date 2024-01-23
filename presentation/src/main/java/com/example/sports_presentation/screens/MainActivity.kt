32fc62c69b2c5c654948c063ee62502607f6c525
varmabhuvnesh <bhuvarma@publicisgroupe.net>
2024-01-23 15:54:01 +0530

    Theming the app

--- presentation/src/main/java/com/example/sports_presentation/screens/MainActivity.kt
+++ presentation/src/main/java/com/example/sports_presentation/screens/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sports_presentation.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
 import androidx.navigation.compose.rememberNavController
 import com.example.sports_presentation.navigation.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,9 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavGraph(
-                navController = navController,
-                onBackFromHome = { finish() },
+            MaterialTheme(
+                content = {
+                    NavGraph(
+                        navController = navController,
+                        onBackFromHome = { finish() },
+                    )
+                },
             )
         }
     }
