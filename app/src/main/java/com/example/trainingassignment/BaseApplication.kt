4e2bacdeb22845ef0eda4c105d78509aebf04eb7
varmabhuvnesh <bhuvarma@publicisgroupe.net>
2024-01-24 15:28:32 +0530

    Removing experimental annotation

--- app/src/main/java/com/example/trainingassignment/BaseApplication.kt
+++ app/src/main/java/com/example/trainingassignment/BaseApplication.kt
@@ -2,10 +2,6 @@ package com.example.trainingassignment
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 
-@FlowPreview
-@ExperimentalCoroutinesApi
 @HiltAndroidApp
 open class BaseApplication : Application()
