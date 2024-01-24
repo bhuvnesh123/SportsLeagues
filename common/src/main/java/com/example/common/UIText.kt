47522ef839860fe58e4b54ed44dd76138865adb2
varmabhuvnesh <bhuvarma@publicisgroupe.net>
2024-01-24 13:13:26 +0530

    Using sealed interface instead of sealed class. Sealed interfaces saves a little bit of code and reduce the memory allocated

--- common/src/main/java/com/example/common/UIText.kt
+++ common/src/main/java/com/example/common/UIText.kt
@@ -2,9 +2,9 @@ package com.example.common
 
 import android.content.Context
 
-sealed class UIText {
-    data class DynamicString(val input: String) : UIText()
-    data class StringResource(val id: Int) : UIText()
+sealed interface UIText {
+    data class DynamicString(val input: String) : UIText
+    data class StringResource(val id: Int) : UIText
     companion object {
         fun getText(uiText: UIText, context: Context): String {
             return when (uiText) {
--- domain/src/main/java/com/example/sports_domain/domainmodels/wrapper/ApiResult.kt
+++ domain/src/main/java/com/example/sports_domain/domainmodels/wrapper/ApiResult.kt
@@ -2,14 +2,14 @@ package com.example.sports_domain.domainmodels.wrapper
 
 import com.example.common.UIText
 
-sealed class ApiResult<out T> {
+sealed interface ApiResult<out T> {
 
-    data class Success<out T>(val value: T) : ApiResult<T>()
+    data class Success<out T>(val value: T) : ApiResult<T>
 
     data class GenericError(
         val code: Int? = null,
         val errorMessage: UIText? = null,
-    ) : ApiResult<Nothing>()
+    ) : ApiResult<Nothing>
 
-    data class NetworkError(val errorMessage: UIText) : ApiResult<Nothing>()
+    data class NetworkError(val errorMessage: UIText) : ApiResult<Nothing>
 }
